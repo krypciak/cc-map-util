27de7c9b204e02e8a9fa8baa4bc705c7ababb600
krypek <krypek@tuta.io>
2023-10-02 20:52:38 +0200

    Add stack

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,27 @@
+export class Stack<T> {
+    array: T[] = []
+
+    constructor(array: T[] = []) {
+        this.array = [...array]
+    }
+
+    push(elements: T) {
+        this.array.push(elements)
+    }
+    pop(): T {
+        return this.array.splice(this.array.length - 1, 1)[0]
+    }
+    peek(): T {
+        return this.array.last()
+    }
+    shift(): T | undefined {
+        return this.array.shift()
+    }
+    length(): number {
+        return this.array.length
+    }
+}
+
 export function assert(arg: any, msg: string = ''): asserts arg {
     if (arg != 0 && ! arg) {
         throw new Error(`Assertion failed: ${msg}`)
